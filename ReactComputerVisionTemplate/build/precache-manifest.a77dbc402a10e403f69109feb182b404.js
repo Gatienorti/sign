1e932f7e40c4977ccb04c3dda46347874a8daf43
Gatien Orti-vinas <gatien.orti@gmail.com>
2021-01-14 18:34:33 -0500

    finish

---
+++ ReactComputerVisionTemplate/build/precache-manifest.a77dbc402a10e403f69109feb182b404.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a3f544e4cb27bca7b6778a1a9dce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db75a7004dc463564e6",
+    "url": "/static/css/main.a716a269.chunk.css"
+  },
+  {
+    "revision": "3c3fe9e7c89da1a4ba8a",
+    "url": "/static/js/2.cb7d3aa8.chunk.js"
+  },
+  {
+    "revision": "0b4c429200d74988aa2f1657a298a43f",
+    "url": "/static/js/2.cb7d3aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db75a7004dc463564e6",
+    "url": "/static/js/main.c3faecf5.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "59a3bc29dde393826150f9d583dcb77c",
+    "url": "/static/media/cam.59a3bc29.jpg"
+  },
+  {
+    "revision": "2387b2c618f0bd46e9dc5f47ea003a37",
+    "url": "/static/media/home.2387b2c6.jpg"
+  }
+]);
